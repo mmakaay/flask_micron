799b88b3c56c06e48c95c33e51a88cdeecfdb484
Maurice Makaay <maurice@makaay.nl>
2016-11-02 18:22:32 +0100

    Added jQuery example, adapted from the Flask jQuery example.

---
+++ examples/jqueryexample/jqueryexample.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+    jQuery Example
+    ~~~~~~~~~~~~~~
+
+    A simple application that shows how Flask-Micron and jQuery get along.
+    This example is a reimplementation of the Flask example with the same name.
+
+    :copyright: (c) 2016 by Stichting Fiber Overal
+    :license: BSD, see LICENSE for more details.
+"""
+from flask import Flask, jsonify, render_template, request
+from flask_micron import Micron
+app = Flask(__name__)
+app.secret_key = 'SimpleKeySinceThisIsJustADemo'
+micron = Micron(app)
+
+
+@micron.method('/_add_numbers', csrf=False)
+def add_numbers(numbers):
+    """Sum a list of numbers server side, ridiculous but well..."""
+    total = 0
+    for number in numbers:
+        try:
+            total = total + float(number)
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+    return total
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
